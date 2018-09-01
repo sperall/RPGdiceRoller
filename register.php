539e0365678b52c88396d02699d1f2d9f7788f4d
sperall <42895192+sperall@users.noreply.github.com>
2018-09-01 18:28:15 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://RPGdiceRoller.herokuapp.com/execute.php';
+$BOT_TOKEN = '667707555:AAHTbXtSWMZZuDv3n4FGGHuoIdCRiol1mXc';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
